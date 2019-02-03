306e8c7415d3bad630239c1f3e71645b30834096
AiYiNing <1914420862@qq.com>
2019-02-03 20:44:21 +0800

    birthday

---
+++ birthday.js
@@ -0,0 +1,32 @@
+var Time = {
+  getTodayUnix: function() {
+    var date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date.getTime();
+  },
+
+  getFormatdate: function(birthday) {
+    var today = this.getTodayUnix();
+
+    var date = (today - birthday) / 1000;
+    var tip = '';
+    if (date > 0) {
+      tip = '你已经出生了' + Math.floor(date / 86400) + '天';
+    } else {
+      tip = '请重新输入日期';
+    }
+    return tip;
+  },
+};
+
+Vue.directive('birthday', {
+  bind: function(el, binding) {
+    console.log('1');
+  },
+  update: function(el, binding) {
+    el.innerHTML = Time.getFormatdate(binding.value);
+  },
+});
